677582daabc83ecc8554c907ba877c1ede023f75
Maciej Witowski <witowski.maciej@gmail.com>
2014-06-23 12:12:28 +0200

    Clean up

--- sample-studio/sample/build/source/r/debug/io/filepicker/sample/R.java
+++ sample-studio/sample/build/source/r/debug/io/filepicker/sample/R.java
@@ -44,20 +44,18 @@ public final class R {
         public static final int spinner=0x7f020011;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000d;
-        public static final int button=0x7f090008;
-        public static final int button1=0x7f090007;
-        public static final int editText1=0x7f090005;
-        public static final int filePathtextView=0x7f09000a;
+        public static final int action_settings=0x7f09000b;
+        public static final int button=0x7f090009;
+        public static final int button1=0x7f090008;
+        public static final int editText1=0x7f090006;
         public static final int gridView1=0x7f090001;
-        public static final int inkfileUrltextView=0x7f09000c;
-        public static final int linearLayout1=0x7f090004;
+        public static final int imageResult=0x7f09000a;
+        public static final int linearLayout1=0x7f090005;
         public static final int listView1=0x7f090000;
         public static final int progressBar1=0x7f090002;
-        public static final int textView1=0x7f090006;
-        public static final int textView2=0x7f090009;
-        public static final int textView3=0x7f09000b;
-        public static final int webView1=0x7f090003;
+        public static final int progressBarLoadMore=0x7f090003;
+        public static final int textView1=0x7f090007;
+        public static final int webView1=0x7f090004;
     }
     public static final class layout {
         public static final int activity_file_picker=0x7f030000;
@@ -90,6 +88,5 @@ public final class R {
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
-        public static final int AppThemeFilePicker=0x7f070002;
     }
 }
