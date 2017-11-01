b2c09c3367b2ba811de3c0f6eace1d8a2626d5f2
Shawn Aten <shawnmaten@gmail.com>
2017-11-01 13:37:33 -0500

    Add demo module

---
+++ demo/src/main/java/com/filestack/demo/UploadStatusReceiver.java
@@ -0,0 +1,22 @@
+package com.filestack.demo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.filestack.FileLink;
+import com.filestack.android.FsConstants;
+import com.filestack.android.Selection;
+
+public class UploadStatusReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String status = intent.getStringExtra(FsConstants.EXTRA_STATUS);
+        Selection selection = (Selection) intent.getSerializableExtra(FsConstants.EXTRA_SELECTION);
+        FileLink fileLink = (FileLink) intent.getSerializableExtra(FsConstants.EXTRA_FILE_LINK);
+
+        Log.i("uploadStatus", selection.getName() + " " + status + " "
+                + (fileLink != null ? fileLink.getHandle() : ""));
+    }
+}
