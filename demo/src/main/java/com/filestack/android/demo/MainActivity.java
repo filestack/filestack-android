ad3376bc907166fcd06bece4a1abc601635e0cda
Shawn Aten <shawnmaten@gmail.com>
2018-05-01 17:09:52 -0500

    change to configure picker based on settings

--- demo/src/main/java/com/filestack/android/demo/MainActivity.java
+++ demo/src/main/java/com/filestack/android/demo/MainActivity.java
@@ -2,9 +2,11 @@ package com.filestack.android.demo;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.filestack.Config;
 import com.filestack.android.FsActivity;
@@ -19,8 +22,12 @@ import com.filestack.android.FsConstants;
 import com.filestack.android.Selection;
 import com.filestack.android.internal.Util;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_FILESTACK = RESULT_FIRST_USER;
@@ -38,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
             UploadStatusReceiver receiver = new UploadStatusReceiver(logView);
             LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilter);
         }
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
 
     @Override
@@ -65,15 +74,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void launch(View view) {
         Intent intent = new Intent(this, FsActivity.class);
-        Config config = new Config(
-                getString(R.string.api_key),
-                getString(R.string.return_url),
-                getString(R.string.policy),
-                getString(R.string.signature));
-        intent.putExtra(FsConstants.EXTRA_CONFIG, config);
-        intent.putExtra(FsConstants.EXTRA_AUTO_UPLOAD, true);
-        String[] mimeTypes = {"application/pdf", "image/*", "video/*"};
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        Set<String> sources = sharedPref.getStringSet("upload_sources", null);
+        intent.putExtra(FsConstants.EXTRA_SOURCES, new ArrayList<>(sources));
+
+        boolean autoUpload = sharedPref.getBoolean("auto_upload", false);
+        intent.putExtra(FsConstants.EXTRA_AUTO_UPLOAD, autoUpload);
+
+        String mimeFilter = sharedPref.getString("mime_filter", null);
+        String[] mimeTypes = mimeFilter.split(",");
         intent.putExtra(FsConstants.EXTRA_MIME_TYPES, mimeTypes);
+
+        String apiKey = sharedPref.getString("api_key", null);
+        String policy = sharedPref.getString("policy", null);
+        String signature = sharedPref.getString("signature", null);
+
+        if (apiKey == null) {
+            Toast.makeText(this, R.string.error_no_api_key, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Config config = new Config(apiKey, getString(R.string.return_url), policy, signature);
+        intent.putExtra(FsConstants.EXTRA_CONFIG, config);
+
         startActivityForResult(intent, REQUEST_FILESTACK);
     }
 }
