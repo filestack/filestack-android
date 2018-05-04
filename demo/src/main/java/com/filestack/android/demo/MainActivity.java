dcdaf3356d0de225cf79eeb2b811f6ad6f2e1361
Shawn Aten <shawnmaten@users.noreply.github.com>
2018-05-04 15:09:12 -0500

    Update demo app (#175)
    * add settings activity and log
    
    * change to configure picker based on settings

--- demo/src/main/java/com/filestack/android/demo/MainActivity.java
+++ demo/src/main/java/com/filestack/android/demo/MainActivity.java
@@ -2,22 +2,36 @@ package com.filestack.android.demo;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.filestack.Config;
 import com.filestack.android.FsActivity;
 import com.filestack.android.FsConstants;
 import com.filestack.android.Selection;
+import com.filestack.android.internal.Util;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_FILESTACK = RESULT_FIRST_USER;
+    private static final int REQUEST_SETTINGS = REQUEST_FILESTACK + 1;
     private static final String TAG = "MainActivity";
 
     @Override
@@ -27,9 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) {
             IntentFilter intentFilter = new IntentFilter(FsConstants.BROADCAST_UPLOAD);
-            UploadStatusReceiver receiver = new UploadStatusReceiver();
+            TextView logView = findViewById(R.id.log);
+            UploadStatusReceiver receiver = new UploadStatusReceiver(logView);
             LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilter);
         }
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
 
     @Override
@@ -50,23 +67,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void openFilestack(View view) {
-        Intent intent = new Intent(this, FsActivity.class);
-        Config config = new Config(
-                getString(R.string.api_key),
-                getString(R.string.return_url),
-                getString(R.string.policy),
-                getString(R.string.signature));
-        intent.putExtra(FsConstants.EXTRA_CONFIG, config);
-        intent.putExtra(FsConstants.EXTRA_AUTO_UPLOAD, true);
-        String[] mimeTypes = {"application/pdf", "image/*", "video/*"};
-        intent.putExtra(FsConstants.EXTRA_MIME_TYPES, mimeTypes);
-        startActivityForResult(intent, REQUEST_FILESTACK);
+    public void settings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivityForResult(intent, REQUEST_SETTINGS);
     }
 
-    public void openFileBrowser(View view) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        startActivityForResult(intent, 0);
+    public void launch(View view) {
+        Intent intent = new Intent(this, FsActivity.class);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        Set<String> sources = sharedPref.getStringSet("upload_sources", null);
+        intent.putExtra(FsConstants.EXTRA_SOURCES, new ArrayList<>(sources));
+
+        boolean autoUpload = sharedPref.getBoolean("auto_upload", false);
+        intent.putExtra(FsConstants.EXTRA_AUTO_UPLOAD, autoUpload);
+
+        String mimeFilter = sharedPref.getString("mime_filter", null);
+        String[] mimeTypes = mimeFilter.split(",");
+        intent.putExtra(FsConstants.EXTRA_MIME_TYPES, mimeTypes);
+
+        String apiKey = sharedPref.getString("api_key", null);
+        String policy = sharedPref.getString("policy", null);
+        String signature = sharedPref.getString("signature", null);
+
+        if (apiKey == null) {
+            Toast.makeText(this, R.string.error_no_api_key, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Config config = new Config(apiKey, getString(R.string.return_url), policy, signature);
+        intent.putExtra(FsConstants.EXTRA_CONFIG, config);
+
+        startActivityForResult(intent, REQUEST_FILESTACK);
     }
 }
--- demo/src/main/java/com/filestack/android/demo/UploadStatusReceiver.java
+++ demo/src/main/java/com/filestack/android/demo/UploadStatusReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.filestack.FileLink;
 import com.filestack.android.FsConstants;
@@ -14,16 +15,24 @@ import java.util.Locale;
 public class UploadStatusReceiver extends BroadcastReceiver {
     private static final String TAG = "UploadStatusReceiver";
 
+    private TextView logView;
+
+    public UploadStatusReceiver(TextView logView) {
+        this.logView = logView;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Locale locale = Locale.getDefault();
         String status = intent.getStringExtra(FsConstants.EXTRA_STATUS);
         Selection selection = intent.getParcelableExtra(FsConstants.EXTRA_SELECTION);
         FileLink fileLink = (FileLink) intent.getSerializableExtra(FsConstants.EXTRA_FILE_LINK);
-
         String name = selection.getName();
         String handle = fileLink != null ? fileLink.getHandle() : "n/a";
-        String msg = String.format(locale, "upload %s: %s (%s)", status, name, handle);
-        Log.i(TAG, msg);
+
+        logView.append("========================\n");
+        logView.append(status.toUpperCase() + "\n");
+        logView.append(name + "\n");
+        logView.append("https://cdn.filestackcontent.com/" + handle + "\n");
+        logView.append("========================\n");
     }
 }
